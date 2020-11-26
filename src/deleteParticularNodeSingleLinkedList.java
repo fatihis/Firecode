6b7bf8f58507fc2f30744abfaedfbef6da816030
fatihis <isk.fatih1@gmail.com>
2020-11-26 19:44:48 +0300

    Add deleteParticularNodeSingleLinkedList and deleteParticularNodeSingleLinkedListTestClass

--- src/deleteParticularNodeSingleLinkedList.java
+++ src/deleteParticularNodeSingleLinkedList.java
@@ -1,20 +1,25 @@
 public class deleteParticularNodeSingleLinkedList {
     public static ListNode deleteAtMiddle(ListNode head, int position) {
         /*Given a singly-linked list, implement a method to delete the node at a given position (starting from 1 as the head position) and return the head of the list. Do nothing if the input position is out of range.*/
-        if(head == null){
+        if (head == null || head.next == null) {
             return null;
+        }
+        if (position == 1) {
+            return head.next;
         }
         ListNode ptr = head;
         int cnt = 1;
-        while(ptr.next != null){
-            if(cnt == position-1){
+        while (ptr.next != null) {
+
+            if (cnt == position - 1) {
                 ptr.next = ptr.next.next;
                 return head;
             }
-            ptr = ptr.next;
             cnt++;
+            ptr = ptr.next;
         }
 
-        return null;
+        return head;
+
     }
 }
